b428809bc1a8dccf49ace6ffa946caef7710158a
tom_glb <goodMoring_glb@163.com>
2019-12-09 14:39:26 +0800

    commit num_0014

--- src/num_0014/LongestCommonPrefix.java
+++ src/num_0014/LongestCommonPrefix.java
@@ -29,8 +29,23 @@ public class LongestCommonPrefix {
         return strs[0];
     }
 	
+	public static String longestCommonPrefix1(String[] strs) {
+		if(strs == null || strs.length == 0) return "";
+		if(strs.length == 1) return strs[0];
+		int count = 0;
+		for(int i=0; i<strs[0].length(); i++){
+			char c = strs[0].charAt(i);
+			for(int j=1; j<strs.length; j++){
+				count = i;
+				if(i == strs[j].length() || strs[j].charAt(i) != c) break;
+			}
+		}
+		return strs[0].substring(0, count+1);
+	}
+	
+	
 	public static void main(String[] args) {
-		String[] strs = {"flowwer","flowwaaa","flowwight"};
+		String[] strs = {"flowwwer","floww","flowwight"};
 		String result = longestCommonPrefix(strs);
 		System.out.println(result);
 	}
